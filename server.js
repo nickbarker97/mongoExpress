8eb65c4e8b35713120b1c359ce26174582e57853
(s) Nicholas Barker <nbarker1@pm7-0274.uopnet.plymouth.ac.uk>
2020-03-04 12:56:50 +0000

    added mongodb basic code

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test').then(()=>{
+    console.log('Connected to DB')
+})
+.catch(err =>{
+    console.log('Not Connected to DB')
+})
+
 
 app.get('/', (req, res)=>{
     res.send('This is the Home Page')
